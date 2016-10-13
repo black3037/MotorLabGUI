8d9c38fa11a4ead7c558fc1aa2cedbe900fedce1
black3037 <black@ksu.edu>
2016-10-13 14:45:54 -0500

    Added Files

---
+++ MotorLabMainWindow.py
@@ -0,0 +1,114 @@
+# ********************************IMPORTS************************************#
+# ***************************************************************************#
+from PyQt4 import QtGui
+
+from PyQt4.QtGui import QMainWindow, QColor, QFileDialog
+
+from MotorLab import Ui_Motorlab
+
+from plot_tools import plot_tools
+
+import os
+# ***************************************************************************#
+# ***************************************************************************#
+
+class MotorLabMainWindow(QMainWindow, Ui_Motorlab):
+    def __init__(self):
+        
+        # Set up the MotorLab Ui
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        
+        # Push Button Signal --> Slot
+        self.JogUpButton.clicked.connect(self.jogup)
+        self.JogDownButton.clicked.connect(self.jogdown)
+        self.StepModelButton.clicked.connect(self.get_step)
+        self.BodeModelButton.clicked.connect(self.get_bode)
+        self.OpenDirectoryButton.clicked.connect(self.open_directory)
+        self.StartButton.toggled.connect(self.start)
+        self.GenerateFileButton.clicked.connect(self.create_csv_file)
+        
+    def get_step(self):
+        self.get_graph = plot_tools()
+        num,den = self.transferfunction()
+        
+        if self.PlotAutoFormatCheckBox.isChecked(): self.get_graph.stepmodel2(num,den)
+        else: self.get_graph.stepmodel(num,den)
+        
+    def get_bode(self):
+        num,den = self.transferfunction()
+        self.get_graph = plot_tools()
+        
+        if self.PlotAutoFormatCheckBox.isChecked(): self.get_graph.bode2(num,den)
+        else: self.get_graph.bode(num,den)
+            
+        
+    def change_directory(self,current_directory):
+        
+        text,ok = QtGui.QInputDialog.getText(self,'Change Working Directory','Enter the full path to working directory:')
+        output_text_to_dataexplorer = 'Directory Changed To: \n' + text        
+        self.DataExplorer.setText(output_text_to_dataexplorer)
+
+    def create_csv_file(self):
+        
+        self.DataExplorer.clear()
+        output_string = str(self.FileName.text()) + '.csv'
+        self.DataExplorer.setText('Generating  ' + output_string + '\n' + 'Success!')   
+        
+        fileout = open(output_string,"w")
+        fileout.write('Hello World')
+        
+    def get_current_working_directory(self):
+        return os.curdir
+        
+    def jogdown(self):
+        
+        # Get the current step size and current command
+        step = float(self.StepSize.text())
+        command = float(self.Command.text())
+        
+        # Incrememnt command based on stepsize and print to screen
+        increment_down = str(command - step)
+        self.Command.setText(increment_down)
+        
+    def jogup(self):
+        
+        # Get the current step size and current command
+        step = float(self.StepSize.text())
+        command = float(self.Command.text())
+        
+        # Incrememnt command based on stepsize and print to screen
+        increment_up = str(command + step)
+        self.Command.setText(increment_up)
+        
+    def open_directory(self):
+    
+        if sys.platform == 'win32':
+            current_working_directory = self.get_current_working_directory()
+            os.startfile(current_working_directory)
+        #TODO: Have this work for Linux and OSX
+    
+    def start(self,checked):
+        
+        if checked: self.StartButton.setText('Stop')
+        elif not checked: self.StartButton.setText('Start')
+            
+    def transferfunction(self):
+    
+        numerator = str(self.Numerator.text())
+        denominator = str(self.Denominator.text())
+        
+        num = map(float,numerator.split(","))
+        den = map(float,denominator.split(","))
+        
+        return num,den
+            
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    app.setStyle('cleanlooks')
+    window = MotorLabMainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
---
+++ plot_tools.py
@@ -0,0 +1,116 @@
+"""
+
+Simple plotting tools for the MotorLab GUI
+
+
+"""
+# ********************************IMPORTS************************************#
+# ***************************************************************************#
+from PyQt4.QtGui import QMainWindow
+
+from MotorLab import Ui_Motorlab
+
+import matplotlib.pyplot as plt
+
+
+
+from scipy import signal
+# ***************************************************************************#
+# ***************************************************************************#
+
+class plot_tools(QMainWindow, Ui_Motorlab):
+    def __init__(self):        
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        
+    def bode(self,num,den):
+        
+        w, mag, phase = signal.bode((num,den))
+        
+        # Plot Formatting
+        plt.ion()
+        plt.close(2)
+        f, axarr = plt.subplots(2, sharex=True)
+        plt.subplot(211)
+        plt.semilogx(w,mag,'dodgerblue')
+        plt.title('Bode Diagram')
+        plt.ylabel('Magnitude (dB)')
+        plt.grid(True, which="both")
+        plt.subplot(212)
+        plt.semilogx(w, phase,'dodgerblue')  
+        plt.ylabel('Phase (Deg)')
+        plt.xlabel('Frequency (rad/s)')
+        plt.grid(True, which="both")
+        plt.figure(2)
+        
+        return plt.show()
+    
+    def bode2(self,num,den):
+        
+        w, mag, phase = signal.bode((num,den))
+        
+        # Plot formatting
+        plt.ion()
+        plt.close(2)
+        plt.rc('font', family='serif')
+        plt.rc('xtick', labelsize='x-small')
+        plt.rc('ytick', labelsize='x-small')     
+        f, axarr = plt.subplots(2, sharex=True)
+        plt.subplot(211)
+        plt.semilogx(w,mag,'black')
+        plt.title('Bode Diagram')
+        plt.ylabel('Magnitude (dB)')
+        plt.grid(True, which="both")
+        plt.subplot(212)
+        plt.semilogx(w, phase,'black')  
+        plt.ylabel('Phase (Deg)')
+        plt.xlabel('Frequency (rad/s)')
+        plt.grid(True,which="both")
+        plt.figure(2)
+        
+        return plt.show()
+    
+    def stepmodel(self,num,den):
+                
+        t, y = signal.step2((num,den))
+        
+        #TODO: Note this messes up for unstable or oscillatory systems
+        get_last_value = y[-1]
+        dcgain = [get_last_value]*len(t)
+     
+        # Plot Formatting
+        plt.ion()
+        plt.close(1)
+        plt.plot(t,y,'dodgerblue',t,dcgain,'k:')
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Amplitude')
+        plt.title('Step Response')
+        plt.figure(1)
+        
+        return plt.show()
+            
+    def stepmodel2(self,num,den):
+        
+        t, y = signal.step2((num,den))
+        
+        #TODO: Note this messes up for unstable or oscillatory systems
+        get_last_value = y[-1]
+        dcgain = [get_last_value]*len(t)
+        
+        # Plot Formatting
+        plt.ion()
+        plt.close(1)
+        plt.rc('font', family='serif')
+        plt.rc('xtick', labelsize='x-small')
+        plt.rc('ytick', labelsize='x-small')            
+        plt.plot(t,y,'black',label='Model')
+        plt.plot(t,dcgain,'k:',label='Command')
+        plt.fill_between(t,y,dcgain,color='lightgrey',alpha=0.3)
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Amplitude')
+        plt.title('Step Response')
+        plt.legend(loc='lower right')
+        plt.figure(1)
+        
+        return plt.show()
+          
